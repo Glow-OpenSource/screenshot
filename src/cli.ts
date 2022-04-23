fafb8370f892fa0c3bd33e24ee7650b75ecd25b9
Googlefan256 <yyykakeru@gmail.com>
2022-04-23 10:38:33 +0900

    fix: lint rules

--- src/cli.ts
+++ src/cli.ts
@@ -8,17 +8,17 @@ import { resolve } from "node:path";
 const args = argv.slice(2);
 
 (async () => {
-	if (!args[0]) return console.log(`${chalk.blue("Info: ")}url missing`);
-	const browser = new ScreenShot();
-	const buf = await browser.screenshot(args[0]).catch(() => null);
-	await browser.destroy();
-	if (!buf)
-		return console.log(
-			`${chalk.red(
-				"Error: "
-			)}error getting screenshot.Please check if your url is correct`
-		);
-	const path = resolve(args[1] || "image.png");
-	await writeFile(path, buf);
-	return console.log(`${chalk.blue("Info: ")}written as ${chalk.green(path)}`);
+  if (!args[0]) return console.log(`${chalk.blue("Info: ")}url missing`);
+  const browser = new ScreenShot();
+  const buf = await browser.screenshot(args[0]).catch(() => null);
+  await browser.destroy();
+  if (!buf)
+    return console.log(
+      `${chalk.red(
+        "Error: "
+      )}error getting screenshot.Please check if your url is correct`
+    );
+  const path = resolve(args[1] || "image.png");
+  await writeFile(path, buf);
+  return console.log(`${chalk.blue("Info: ")}written as ${chalk.green(path)}`);
 })();
--- src/index.mjs
+++
@@ -1,6 +0,0 @@
-import _m from "./index.js";
-export default Object.assign(_m.default || {}, _m);
-export const ScreenShot = _m.ScreenShot;
-export const DefaultScreenshotOption = _m.DefaultScreenshotOption;
-export const DefaultScreenShotConstructorOptions =
-	_m.DefaultScreenShotConstructorOptions;
