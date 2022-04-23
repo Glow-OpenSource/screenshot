353c10879303ca6c72ce7d0f3474f0208ab46d4c
Googlefan256 <yyykakeru@gmail.com>
2022-04-23 10:21:55 +0900

    add: keywords

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,6 @@
 import _m from "./index.js";
 export default Object.assign(_m.default || {}, _m);
-export const ScreenShot = _m.ScreenShot
-export const DefaultScreenshotOption = _m.DefaultScreenshotOption
-export const DefaultScreenShotConstructorOptions = _m.DefaultScreenShotConstructorOptions
+export const ScreenShot = _m.ScreenShot;
+export const DefaultScreenshotOption = _m.DefaultScreenshotOption;
+export const DefaultScreenShotConstructorOptions =
+	_m.DefaultScreenShotConstructorOptions;
